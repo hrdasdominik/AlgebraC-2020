02298bacfd44e4231d106b1f7ddcc38d9f5c4f04
hrdasdominik <68448419+hrdasdominik@users.noreply.github.com>
2020-12-07 13:54:03 +0100

    Funkcija equip i unequip armor i weapon napravljena, dodana player klasa

--- Igrica2/Program.cs
+++ Igrica2/Program.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 
 namespace Igrica2
 {
+    class Player
+    {
+        public void fInputName()
+        {
+            Console.WriteLine("Upisite svoje ime:");
+            name = Console.ReadLine();
+        }
+        string name;
+        int gold = 250;
+        int armor = 0;
+        int damage = 0;
+        Inventory inventory = new Inventory();
+    }
+
     class Vendor
     {
         //Functions
@@ -36,8 +50,45 @@ namespace Igrica2
             bag.Add(item);
         }
 
+        //Weapon functions
+        public void fEquipWeapon(Weapon weapon)
+        {
+            int weaponLimit = 2;
+            if (weaponSlot.Count <= weaponLimit)
+                weaponSlot.Add(weapon);
+            else
+                Console.WriteLine("Nemate dovoljno ruku za novo oružje. :P");
+        }
+        public void fUnEquipWeapon(Weapon weapon)
+        {
+            if (weaponSlot.Contains(weapon))
+                weaponSlot.Remove(weapon);
+            else
+                Console.WriteLine("Trenutno ne držite u rukama {0}", weapon.fGetName());
+        }
+
+        //Armor functions
+        public void fEquipArmor(Armor armor)
+        {
+            int armorLimit = 1;
+            if (armorSlot.Count < armorLimit)
+                armorSlot.Add(armor);
+            else
+                Console.WriteLine("Već imate na sebi oklop.");
+        }
+        public void fUnEquipArmor(Armor armor)
+        {
+            if (armorSlot.Contains(armor))
+                armorSlot.Remove(armor);
+            else
+                Console.WriteLine("Trenutno niste opremljeni s {0}", armor.fGetName());
+        }
+
 
         List<Item> bag = new List<Item>();
+        List<Weapon> weaponSlot = new List<Weapon>();
+        List<Armor> armorSlot = new List<Armor>();
+
     }
 
     class Item
@@ -97,13 +148,14 @@ namespace Igrica2
     {
         static void Main(string[] args)
         {
+            Player player = new Player();
+            player.fInputName();
+
             Weapon axe = new Weapon("Axe", 8, 10, 50);
             Weapon sword = new Weapon("Sword", 6, 5, 35);
 
             Armor plate = new Armor("Plate", 18, 25, 100);
             Armor leather = new Armor("Leather", 14, 8, 40);
-
-            Inventory inventory = new Inventory();
             
             Vendor vendor = new Vendor();
             vendor.fAddItem(axe);
